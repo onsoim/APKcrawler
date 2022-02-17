3bf8d61aa223dd7664e7c5472c1a9a690d4d881f
onsoim <onsoim@gmail.com>
2022-02-17 00:28:15 -0800

    [EDIT] adb related job as seperated process

--- src/APKcrawler.py
+++ src/APKcrawler.py
@@ -2,7 +2,7 @@
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from ADB.ADB import ADB
+from ADB.ADB import ADB                         #
 from configparser import ConfigParser
 from datetime import datetime
 from GooglePlay.GooglePlay import GOOGLEPLAY    #
@@ -11,7 +11,26 @@ from SQLite.SQLite import SQLITE                #
 from time import *
 
 
-if __name__ == "__main__":
+def jobA():
+    adb = ADB()
+    sqlite = SQLITE()
+
+    while True:
+        for package in sqlite.read({"extract_date": None, "install_date": "NOT NULL"}):
+            adb.setAPK(package)
+            if adb.is_installed() and adb.pull() and adb.uninstall():
+                sqlite.update(
+                    set = {
+                        "extract_date": datetime.now()
+                    }, 
+                    where = {
+                        "package_name": package
+                    }
+                )
+                print(f"[3/3] Done {package}")
+        sleep(3)
+
+def main():
     parser = ConfigParser()
     parser.read('./GooglePlay/config.ini')
 
@@ -21,33 +40,26 @@ if __name__ == "__main__":
     traversal = Process(target=gp.traversal, args = (packages,))
     traversal.start()
 
+    adb = Process(target=jobA)
+    adb.start()
+
     sqlite = SQLITE()
-    adb = ADB()
 
     while True:
-        try:
-            for package in packages.get():
-                sqlite.create(package)
-            # for package in sqlite.read({"install_date": None}):
-                if gp.install(package):
-                    sqlite.update(
-                        set = {
-                            "install_date": datetime.now()
-                        }, 
-                        where = {
-                            "package_name": package
-                        }
-                    )
-            for package in sqlite.read({"extract_date": None, "install_date": "NOT NULL"}):
-                adb.setAPK(package)
-                if adb.is_installed() and adb.pull() and adb.uninstall():
-                    print(f"[3/3] Done {package}")
-                    sqlite.update(
-                        set = {
-                            "extract_date": datetime.now()
-                        }, 
-                        where = {
-                            "package_name": package
-                        }
-                    )
+        for package in packages.get():
+            sqlite.create(package)
+        # for package in sqlite.read({"install_date": None}):
+            if gp.install(package):
+                sqlite.update(
+                    set = {
+                        "install_date": datetime.now()
+                    },
+                    where = {
+                        "package_name": package
+                    }
+                )
+
+if __name__ == "__main__":
+    while True:
+        try: main()
         except Exception as e: print(e)
