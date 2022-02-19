689b5a7e308e52e3ff62faa9f568440c6ed8ff40
onsoim <onsoim@gmail.com>
2022-02-19 15:33:25 +0900

    [ADD] waiter to prevent event before loading is finished

--- src/GooglePlay/Google.py
+++ src/GooglePlay/Google.py
@@ -1,12 +1,15 @@
 
 import undetected_chromedriver.v2 as uc     # pip3 install undetected-chromedriver
 from selenium.webdriver.common.by import By # pip3 install selenium
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class GOOGLE:
     def __init__(self):
         self.driver = uc.Chrome()
         self.driver.get("https://accounts.google.com/signin/v2/identifier?continue=https://www.google.com")
+        self.wait   = WebDriverWait(self.driver, 60)
 
     def login(self, config):
         self.ID = config['ID']
@@ -16,11 +19,13 @@ class GOOGLE:
         self.setPassword()
 
     def setIdentifierId(self):
+        self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="identifierId"]')))
         self.driver.find_element(By.XPATH, '//*[@id="identifierId"]').send_keys(self.ID)
         self.driver.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button').click()
         self.driver.implicitly_wait(2)
 
     def setPassword(self):
+        self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input')))
         self.driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input').send_keys(self.PW)
         self.driver.find_element(By.XPATH, '//*[@id="passwordNext"]/div/button').click()
         self.driver.implicitly_wait(2)
